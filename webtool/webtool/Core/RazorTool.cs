655b178eca677366978605a803af7428b9ca299d
yuxiang <yuxiangs0911@gmail.com>
2015-03-27 17:01:24 +0800

    111111

--- webtool/webtool/Core/RazorTool.cs
+++ webtool/webtool/Core/RazorTool.cs
@@ -14,22 +14,34 @@ namespace webtool
             ignoreDirectory = "App_Data,bin,Shared,Properties,obj,aspnet_client," + ignoreDirectory;
         }
 
-        private static void BuildImpl(DirectoryInfo projectDirectoryInfo, string ignoreDirectory)
+        private static void BuildImpl(DirectoryInfo projectDirectoryInfo, DirectoryInfo currentDirectoryInfo, DirectoryInfo outputDirectoryInfo, string ignoreDirectory)
         {
+            foreach (var file in projectDirectoryInfo.GetFiles())
+            {
+                if (file.Extension == ".cshtml")
+                {
+
+                }
+                else
+                {
+                    string targetPath = Path.Combine(outputDirectoryInfo.FullName, projectDirectoryInfo.FullName.Substring(currentDirectoryInfo.FullName.Length + 1));
+                    DirectoryInfo targetDirInfo = DirectoryTool.CreateDirectory(targetPath);
+                    File.Copy(file.FullName, targetDirInfo.FullName, true);
+                }
+            }
+
             DirectoryInfo[] directories = projectDirectoryInfo.GetDirectories();
             foreach (var dir in directories)
             {
+                // ignore folder
                 if (ignoreDirectory.Contains(dir.Name))
                 {
                     continue;
                 }
-                foreach (var file in dir.GetFiles())
-                {
-                    
-                }
-                BuildImpl(projectDirectoryInfo, ignoreDirectory);
+                BuildImpl(projectDirectoryInfo, dir, outputDirectoryInfo, ignoreDirectory);
             }
         }
+
 
     }
 }
