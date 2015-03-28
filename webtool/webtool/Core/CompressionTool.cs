907e5182ba44d5cbe7170d882a50aa6f0222436a
yuxiang <yuxiangs0911@gmail.com>
2015-03-28 17:46:53 +0800

    22
    22

--- webtool/webtool/Core/CompressionTool.cs
+++ webtool/webtool/Core/CompressionTool.cs
@@ -11,7 +11,23 @@ namespace webtool
     {
         public static void Compress(DirectoryInfo projectDirectoryInfo, Structure structure)
         {
+            DirectoryInfo cssDirInfo = DirectoryTool.CreateDirectory(Path.Combine(projectDirectoryInfo.FullName, structure.css));
+            DirectoryInfo scriptDirInfo = DirectoryTool.CreateDirectory(Path.Combine(projectDirectoryInfo.FullName, structure.script));
+        }
 
+        private static void MergeCss(DirectoryInfo cssDirInfo)
+        {
+            var cssFiles = cssDirInfo.GetFiles();
+            if (cssFiles.Length < 2)
+            {
+                return;
+            }
+            var firstCssFile = cssFiles[0];
+            for (int i = 1; i < cssFiles.Length; i++)
+            {
+                var cssFile = cssFiles[i];
+
+            }
         }
     }
 }
---
+++ webtool/webtool/Core/ProcessTool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webtool
+{
+    public class ProcessTool
+    {
+        public static string ExecuteCommand(string command)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.StandardInput.WriteLine(command + "&exit");
+            p.StandardInput.AutoFlush = true;
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+            return output;
+        }
+    }
+}
